088de8fd7b0df189d0dbe2e7af03625366e07357
unknown <rnrudxo2872@naver.com>
2021-08-20 15:13:00 +0900

    project init

---
+++ src/main/java/com/hipsters/hipsters/HipstersApplication.java
@@ -0,0 +1,13 @@
+package com.hipsters.hipsters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipstersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipstersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hipsters/hipsters/HipstersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipsters.hipsters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipstersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
