f677d5abbe02508d1041d2ee9e01dc7334a652af
unknown <rnrudxo2872@naver.com>
2021-08-24 15:55:16 +0900

    잠시 커밋

--- src/main/java/com/dosters/todo/service/ToDoService.java
+++ src/main/java/com/dosters/todo/service/ToDoService.java
@@ -8,7 +8,6 @@ import com.dosters.todo.repository.ToDoRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
