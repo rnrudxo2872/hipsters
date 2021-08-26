17626db6b7137ea18e453a77e2b5dd4714ef82a7
unknown <rnrudxo2872@naver.com>
2021-08-26 17:31:42 +0900

    ToDoRestController 유닛 테스트 중

---
+++ src/main/java/com/dosters/api/todo/service/ToDoService.java
@@ -0,0 +1,8 @@
+package com.dosters.api.todo.service;
+
+import com.dosters.dtos.CommonOutput;
+import com.dosters.interfaces.baseService;
+import com.dosters.todo.domain.ToDoEntity;
+
+public interface ToDoService extends baseService<ToDoEntity, CommonOutput> {
+}
--- src/main/java/com/dosters/interfaces/baseJpaFunc.java
+++
@@ -1,7 +0,0 @@
-package com.dosters.interfaces;
-
-import java.util.List;
-
-public interface baseJpaFunc {
-    // public List<E> getList(Long id);
-}
