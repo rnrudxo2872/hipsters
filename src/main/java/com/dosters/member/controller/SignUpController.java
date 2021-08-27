d04fcccc0cbb749e56e288b57ff0cdd2241b6d67
김홍주 <hong_j@gimhongjuui-MacBookPro.local>
2021-08-27 13:44:53 +0900

    1

---
+++ src/main/java/com/dosters/member/controller/SignUpController.java
@@ -0,0 +1,18 @@
+package com.dosters.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignUpController {
+    
+    @GetMapping("")
+    public String signup() {
+        
+        return "member/signUp";
+    }
+
+
+}
